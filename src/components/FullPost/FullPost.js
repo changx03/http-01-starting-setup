12eddd0ef922e126442777b373520b38e9378556
Luke Chang <luke@csel.co.nz>
2018-02-27 17:10:53 +1300

    initial commit

---
+++ src/components/FullPost/FullPost.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './FullPost.css';
+
+class FullPost extends Component {
+  state = {
+    loadedPost: null,
+  };
+
+  componentDidUpdate(prevProps) {
+    console.log('[FullPost] componentDidUpdate', prevProps.id, this.props.id);
+    if (prevProps.id !== this.props.id) {
+      axios
+        .get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+        .then(response => {
+          console.log('[FullPost]', response);
+          this.setState({
+            loadedPost: response.data,
+          });
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  }
+
+  render() {
+    let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
+    if (this.state.loadedPost) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
+          <div className="Edit">
+            <button className="Delete">Delete</button>
+          </div>
+        </div>
+      );
+    } else if (this.props.id) {
+      post = <p style={{ textAlign: 'center' }}>Loading...</p>;
+    }
+    return post;
+  }
+}
+
+export default FullPost;
---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+  state = {
+    posts: [],
+    selectedId: null,
+  };
+
+  componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        console.log('[Blog]', response);
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => ({
+          ...post,
+          author: 'Max',
+        }));
+        this.setState({
+          posts: updatedPosts,
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  render() {
+    const posts = this.state.posts.map(post => (
+      <Post
+        key={post.id}
+        title={post.title}
+        author={post.author}
+        onClick={() => this._onPostClicked(post.id)}
+      />
+    ));
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
+
+  _onPostClicked = id => {
+    this.setState({
+      selectedId: id,
+    })
+  };
+}
+
+export default Blog;
