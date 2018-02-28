0a83b09934b8218f0d6a03d1453889826d7675e0
changx03 <luke.x.chang@gmail.com>
2018-03-01 03:00:32 +1300

    Added axios instances

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+// axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'test token';
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export const globalInterceptorReq = axios.interceptors.request.use(request => {
   console.log(request);
